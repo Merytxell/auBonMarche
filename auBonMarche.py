6507102fb3faa7ee782b5baab9adf0b42fffe36d
AvilaC <avila.claire@gmail.com>
2025-03-12 09:52:05 +0100

    ajout de la liste des fruits et légumes et affichage

--- auBonMarche.py
+++ auBonMarche.py
@@ -5,6 +5,14 @@ class Vegetable:
         self.price=price
         self.stock=stock
 
+    def __str__(self):
+        return f"{self.name}-{self.price}€,{self.stock}kg en stock"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+#créer une liste de fruits et légumes
 vegetables_list =[
     Vegetable("Clémentine",2.90, 6),
     Vegetable("Datte", 3.50,4),
@@ -17,12 +25,24 @@ vegetables_list =[
     Vegetable("Poire", 2.50, 5),
     Vegetable("Pomme", 1.50,8),
     Vegetable("Carotte",1.30,7),
-
+    Vegetable("Choux de Bruxelles", 4, 4),
+    Vegetable("Chou vert",2.5,12),
+    Vegetable("Courge Butternut", 2.50, 6),
+    Vegetable("Endive",2.50,5),
+    Vegetable("Epinard",2.60,4),
+    Vegetable("Poireau",1.20,5),
+    Vegetable("Potiron", 2.50,6),
+    Vegetable("Radis noir",5,10),
+    Vegetable("Salsifis",2.5,3)
 ]
 
-#créer une liste de fruits et légumes
+
 #créer un menu où l'utilisateur pourra choisir ses achats
+
+
 #mettre à jour un stock pour chaque produit
+for vegetable in vegetables_list:
+    print(vegetable)
 #afficher la liste des clients avec total achat
 #stock total de chaque légumes
 
