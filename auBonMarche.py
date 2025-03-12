e204a38bb941150ad8ca3b357f6bba1f76f13159
AvilaC <avila.claire@gmail.com>
2025-03-12 09:24:54 +0100

    inital commit

---
+++ auBonMarche.py
@@ -0,0 +1,28 @@
+#definir une classe fruit/legumes
+class Vegetable:
+    def __init__ (self,name,price,stock):
+        self.name= name
+        self.price=price
+        self.stock=stock
+
+vegetables_list =[
+    Vegetable("Clémentine",2.90, 6),
+    Vegetable("Datte", 3.50,4),
+    Vegetable("Grenade",3.50,3),
+    Vegetable("Kaki",4.50,3),
+    Vegetable("Kiwi",3.50,5),
+    Vegetable("Mandarine",2.80,6),
+    Vegetable("Orange",1.50,8),
+    Vegetable("Pamplemousse",2,8),
+    Vegetable("Poire", 2.50, 5),
+    Vegetable("Pomme", 1.50,8),
+    Vegetable("Carotte",1.30,7),
+
+]
+
+#créer une liste de fruits et légumes
+#créer un menu où l'utilisateur pourra choisir ses achats
+#mettre à jour un stock pour chaque produit
+#afficher la liste des clients avec total achat
+#stock total de chaque légumes
+
